34f10f396c3246c4b74369a3af0fe1c70748c619
christina-gardash <christina.gardash@gmail.com>
2023-06-16 10:55:04 +0200

    added nav from daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
+import Nav from "./components/Nav";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <br /><br />
+    <div className="App min-h-screen">
 
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <Nav/>
+      <main>main</main>
+      <footer>footer</footer>
+
     </div>
   );
 }
